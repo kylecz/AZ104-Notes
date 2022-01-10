ba7f2e306323375190e5e80b4baf50983f37f5aa
Kyle Czernuszka <czernuka@outlook.com>
2022-01-09 19:50:11 -0500

    Initial commit

--- Networking/AZ-104 - Networking.ps1
+++ Networking/AZ-104 - Networking.ps1
@@ -3,3 +3,7 @@ $ServiceTags = Get-AzNetworkServiceTag -Location canadacentral
 $ServiceTags.Values
 ($serviceTags.Values | Where-Object { $_.Name -eq "Storage.CanadaCentral" }).Properties.AddressPrefixes
 ($serviceTags.Values | Where-Object { $_.Name -eq "Storage.CanadaCentral" }).Properties.AddressPrefixes.count
+
+New-AzResourceGroup -Name "Kyle-RG-02" -Location "canadacentral"
+New-AzStorageAccount -ResourceGroupName "Kyle-RG-02" -AccountName "kylestorageaccount01" -Location canadacentral -SkuName Standard_GRS
+nslookup kylestorageaccount01.blob.core.windows.net
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Repository of useful tidbits as you study for the AZ-104 exam!
 
 Kyle
+
+https://github.com/kylecz/AZ104-Notes
